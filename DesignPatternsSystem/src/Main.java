109d791deb409b3a59b1ac609a56c7f7dd3d22b5
JackTeahan98 <jteahan98@gmail.com>
2020-01-01 19:38:05 +0000

    Singleton Implemented
    Shop class introduced with methods that determine if the shop is open, closed or closed for lunch. Purchasing phones or deals cannot take place when shop is closed.

--- DesignPatternsSystem/src/Main.java
+++ DesignPatternsSystem/src/Main.java
@@ -1,19 +1,51 @@
 import javax.swing.*;
 import java.sql.*;
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, ParseException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","jack","jack");
 
+        Shop phoneShop = Shop.getInstance();
         System.out.println("********** Welcome to Phone Shop! **********\n");
+        phoneShop.openShop();
+
+        Date date = new Date();
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTime(date);
+        calendar1.set(Calendar.HOUR, 1);
+        calendar1.set(Calendar.MINUTE,0);
+        calendar1.set(Calendar.SECOND,0);
+
+        Date date2 = new Date();
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(date2);
+        calendar2.set(Calendar.HOUR, 2);
+        calendar2.set(Calendar.MINUTE,0);
+        calendar2.set(Calendar.SECOND,0);
+
+        Date date3 = new Date();
+        Calendar calendar3 = Calendar.getInstance();
+        calendar3.setTime(date3);
 
 
+        Date x = calendar3.getTime();
+        if (x.after(calendar1.getTime()) && x.before(calendar2.getTime())) {
+             phoneShop.closedForLunch();
+        }
 
-        purchasePhone(con);
+        if(phoneShop.shopStatus() == true) {
+            purchasePhone(con);
+        }
 
+        phoneShop.closeShop();
         con.close();
 
     }
